8cbb8e178632eeb3ebdd6777f248bc7e3d743470
k-paris <125871431+k-paris@users.noreply.github.com>
2026-04-27 09:06:16 +0200

    update resize

--- Modules/VM/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_id" {
-  value = nutanix_virtual_machine.vm.metadata.uuid
-    description = "The ID of the created virtual machine."
-}
-
-output "vm_ip_address" {
-  value       = nutanix_virtual_machine.vm.nic_list[0].ip_endpoint_list[0].ip
-  description = "The IP address of the created virtual machine."
-}
